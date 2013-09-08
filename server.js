b2e9c39536dde538d53907e3eff5cfdd5407f329
elf Pavlik <perpetual-tripper@wwelves.org>
2013-09-08 16:19:41 +0200

    implemented updating client

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var http = require('http');
 var querystring = require('querystring');
 var shell = require('shelljs');
+var nconf = require('nconf');
+
+nconf.file({ file: 'config.json'});
 
 var manageHook = function(payload) {
-  console.log(payload);
+  // handle server
+  if(payload.repository.id === nconf.get('server').id){
+    console.log('>>> updating server');
+  }
+
+  // handle client
+  if(payload.repository.id === nconf.get('client').id){
+    console.log('>>> updating client');
+    shell.cd(nconf.get('client').path);
+    shell.exec('git pull');
+    shell.exec('bower install');
+  }
 };
 
 var server = http.createServer(function(req, res){
@@ -19,5 +33,6 @@ var server = http.createServer(function(req, res){
   res.end();
 });
 
-server.listen(5010);
+server.listen(nconf.get('port'));
+console.log('listening on port ' + nconf.get('port'));
 
